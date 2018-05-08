6077998770c5803ba7a7dada06077f6ace7deb11
linian <linian@zbj.com>
2018-05-08 22:19:50 +0800

    update dashboard

---
+++ src/routes/dashboard/components/EquipmentTemperature.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactEcharts from 'echarts-for-react'
+import PropTypes from 'prop-types'
+
+const EquipmentTemperature = ({ dashboard }) => {
+  const getOption = () => ({
+    title: {
+      text: '设备平均温度'
+    },
+    tooltip: {},
+    legend: {
+      data:['设备平均温度']
+    },
+    xAxis: {
+      data: ['区域1', '区域2', '区域3', '区域4']
+    },
+    yAxis: {},
+    series: [{
+      name: '设备平均温度',
+      type: 'bar',
+      data: [5, 20, 36, 10]
+    }]
+  })
+
+  return (
+    <ReactEcharts
+      option={getOption()}
+      style={{height: '300px', width: '100%', backgroundColor: '#fff'}}
+      opts={{ renderer: 'svg' }}/>
+  )
+}
+
+
+EquipmentTemperature.propTypes = {
+  dashboard: PropTypes.object,
+}
+
+
+export default EquipmentTemperature
--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -2,27 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Row, Col } from 'antd'
 import { connect } from 'dva'
-import MapChart from './components/MapChart'
-import WarningChart from './components/WarningChart'
-import PieChart from './components/PieChart'
+// import MapChart from './components/MapChart'
+import OnlineRate from './components/OnlineRate'
+import EquipmentTemperature from './components/EquipmentTemperature'
+import EquipmentHumidity from './components/EquipmentHumidity'
+import Two from './components/Two'
+import styles from './style.less'
 
 const Dashboard = ({
   dashboard,
 }) => {
-
-  return (
+  return (<div>
     <Row gutter={16} style={{ marginTop: '30px' }}>
-      <Col span="6">
-        <WarningChart dashboard={dashboard} />
+      <Col span="12" className={styles.block}>
+        <OnlineRate dashboard={dashboard} title="在线率"/>
       </Col>
-      <Col span="12">
-        <MapChart dashboard={dashboard} />
-      </Col>
-      <Col span="6">
-        <PieChart dashboard={dashboard} />
+      <Col span="12" className={styles.block}>
+        <EquipmentTemperature dashboard={dashboard} />
       </Col>
     </Row>
-  )
+
+    <Row gutter={16} style={{ marginTop: '30px' }}>
+      <Col span="12" className={styles.block}>
+        <EquipmentHumidity dashboard={dashboard} />
+      </Col>
+      <Col span="12" className={styles.block}>
+        <Two dashboard={dashboard} />
+      </Col>
+    </Row>
+  </div>)
 }
 
 Dashboard.propTypes = {
