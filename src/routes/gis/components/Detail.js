786b986c4c42976e56c4d831d797823546a2d4ee
linux_er <linux_er@126.com>
2018-06-22 21:45:27 +0800

    整理样式

--- src/routes/gis/components/Detail.js
+++ src/routes/gis/components/Detail.js
@@ -2,19 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from 'antd'
 
+
 const ListItem = List.Item
 const ListItemMeta = List.Item.Meta
 
+
 const Detail = ({ gis, dispatch }) => {
-  return <List
-    header="设备详情"
-    dataSource={gis.equitmentInfo}
-    renderItem={item => (
-      <ListItem>
-        <ListItemMeta title={item.title} description={item.description}/>
-      </ListItem>
-    )}
-  />
+  return <div className="gis">
+    <List
+      header="设备详情"
+      dataSource={gis.equitmentInfo}
+      renderItem={item => (
+        <ListItem>
+          <ListItemMeta title={item.title} description={item.description}/>
+        </ListItem>
+      )}
+    />
+  </div>
+
 }
 
 
