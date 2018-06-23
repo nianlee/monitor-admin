029e9ef5e7460ef0fcd38bae7080df4aaab6284d
linian <linian@zbj.com>
2018-06-23 22:59:40 +0800

    update gis

--- src/models/gis.js
+++ src/models/gis.js
@@ -1,4 +1,5 @@
 import { queryDeviceSelective } from 'services/gis'
+import { message } from 'antd'
 
 export default {
 
@@ -20,55 +21,59 @@ export default {
   effects: {
     *queryDeviceSelective({ payload }, { call, put, select }) {
       const resData = yield call(queryDeviceSelective, payload)
-      const info = resData.data.rows[0].datDevice
-      const equitmentInfo = []
+      if (resData.success) {
+        const info = resData.data.rows[0].datDevice
+        const equitmentInfo = []
 
-      equitmentInfo.push({
-        key: 1,
-        title: '设备名称',
-        description: info.name,
-      })
+        equitmentInfo.push({
+          key: 1,
+          title: '设备名称',
+          description: info.name,
+        })
 
-      equitmentInfo.push({
-        key: 2,
-        title: '设备类型',
-        description: info.type
-      })
+        equitmentInfo.push({
+          key: 2,
+          title: '设备类型',
+          description: info.type
+        })
 
-      equitmentInfo.push({
-        key: 3,
-        title: '厂商',
-        description: info.manufacturer
-      })
+        equitmentInfo.push({
+          key: 3,
+          title: '厂商',
+          description: info.manufacturer
+        })
 
-      equitmentInfo.push({
-        key: 4,
-        title: '运营商',
-        description: info.carrierOperator
-      })
+        equitmentInfo.push({
+          key: 4,
+          title: '运营商',
+          description: info.carrierOperator
+        })
 
-      equitmentInfo.push({
-        key: 5,
-        title: '设备地址',
-        description: info.detailAddr
-      })
-
-
-      equitmentInfo.push({
-        key: 6,
-        title: '设备状态',
-        description: info.state
-      })
+        equitmentInfo.push({
+          key: 5,
+          title: '设备地址',
+          description: info.detailAddr
+        })
 
 
-      yield put({
-        type: 'updateState',
-        payload: {
-          equitmentInfo: equitmentInfo,
-          longitude: info.longitude,
-          latitude: info.latitude,
-        }
-      })
+        equitmentInfo.push({
+          key: 6,
+          title: '设备状态',
+          description: info.state
+        })
+
+
+        yield put({
+          type: 'updateState',
+          payload: {
+            equitmentInfo: equitmentInfo,
+            longitude: info.longitude,
+            latitude: info.latitude,
+          }
+        })
+      } else {
+        message.error(resData.message)
+      }
     },
   },
 
