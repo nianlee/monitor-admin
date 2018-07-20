59cd68ee49d2130bfead152b2290dace8240d9df
linian <linian@zbj.com>
2018-07-20 09:33:03 +0800

    删除用户信息

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,97 +1,113 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Menu, Layout, Dropdown, Icon } from 'antd'
-import { Link } from 'react-router-dom'
-import UserInfo from './UserInfo'
-import styles from './style.less'
+import React from "react";
+import PropTypes from "prop-types";
+import { Menu, Layout } from "antd";
+import { Link } from "react-router-dom";
+// import UserInfo from "./UserInfo";
+import styles from "./style.less";
 
-const MenuItem = Menu.Item
-const SubMenu = Menu.SubMenu
-const Header = Layout.Header
+const MenuItem = Menu.Item;
+const SubMenu = Menu.SubMenu;
+const Header = Layout.Header;
 
-const MHeader = ({
-  app,
-  location,
-  dispatch,
-}) => {
+const MHeader = ({ app, location, dispatch }) => {
   const getSelectedKeys = menu => {
     if (!menu) {
-      return []
+      return [];
     }
-    const selectedKeys = []
-    const firstLevel = menu.find(item => item.route === location.pathname)
+    const selectedKeys = [];
+    const firstLevel = menu.find(item => item.route === location.pathname);
     if (firstLevel) {
-      selectedKeys.push(firstLevel.id.toString())
-      return selectedKeys
+      selectedKeys.push(firstLevel.id.toString());
+      return selectedKeys;
     }
-    return selectedKeys
-  }
+    return selectedKeys;
+  };
 
-  const showModal = () => {
-    dispatch({ type: 'app/updateState', payload: { userInfoModalVisible: true }})
-    dispatch({ type: 'app/queryUserInfo' })
-  }
+  // const showModal = () => {
+  //   dispatch({
+  //     type: "app/updateState",
+  //     payload: { userInfoModalVisible: true }
+  //   });
+  //   dispatch({ type: "app/queryUserInfo" });
+  // };
 
-  const userMenu = (
-    <Menu style={{ lineHeight: '64px', borderBottom: 'none' }}>
-      <MenuItem>
-        <a target="_blank" rel="noopener noreferrer" onClick={showModal}>详细信息</a>
-      </MenuItem>
-      <MenuItem>
-        <a target="_blank" rel="noopener noreferrer" onClick={loginOut}>退出</a>
-      </MenuItem>
-    </Menu>
-  );
+  // const userMenu = (
+  //   <Menu style={{ lineHeight: "64px", borderBottom: "none" }}>
+  //     <MenuItem>
+  //       <a target="_blank" rel="noopener noreferrer" onClick={showModal}>
+  //         详细信息
+  //       </a>
+  //     </MenuItem>
+  //     <MenuItem>
+  //       <a target="_blank" rel="noopener noreferrer" onClick={loginOut}>
+  //         退出
+  //       </a>
+  //     </MenuItem>
+  //   </Menu>
+  // );
 
-  function loginOut() {
-    dispatch({ type:'app/loginout' })
-  }
+  // function loginOut() {
+  //   dispatch({ type: "app/loginout" });
+  // }
 
   const renderMenu = () => {
-    const genMenuItem = (data) => {
+    const genMenuItem = data => {
       return data.map(item => {
-        return <MenuItem key={item.id}><Link to={item.route}>{item.name}</Link></MenuItem>
-      })
-    }
+        return (
+          <MenuItem key={item.id}>
+            <Link to={item.route}>{item.name}</Link>
+          </MenuItem>
+        );
+      });
+    };
 
     return app.menu.map(item => {
       if (item.children) {
-        return <SubMenu key={item.id} title={item.name}>{genMenuItem(item.children)}</SubMenu>
+        return (
+          <SubMenu key={item.id} title={item.name}>
+            {genMenuItem(item.children)}
+          </SubMenu>
+        );
       }
-      return <MenuItem key={item.id}><Link to={item.route}>{item.name}</Link></MenuItem>
-    })
-  }
+      return (
+        <MenuItem key={item.id}>
+          <Link to={item.route}>{item.name}</Link>
+        </MenuItem>
+      );
+    });
+  };
 
-  const selectedKeys = getSelectedKeys(app.menu)
+  const selectedKeys = getSelectedKeys(app.menu);
 
   return (
     <Header>
       <div className={styles.logo}>智能物联远程设备监控系统</div>
-      <div className={styles.user}>
+      {/* <div className={styles.user}>
         <Dropdown overlay={userMenu}>
           <a>
-            {app.user.userName}<Icon type="down" />
+            {app.user.userName}
+            <Icon type="down" />
           </a>
         </Dropdown>
-      </div>
+      </div> */}
       <Menu
         theme="dark"
         mode="horizontal"
-        defaultSelectedKeys={['2']}
+        defaultSelectedKeys={["2"]}
         selectedKeys={selectedKeys}
-        style={{ lineHeight: '64px', borderBottom: 'none' }}
+        style={{ lineHeight: "64px", borderBottom: "none" }}
       >
         {renderMenu()}
       </Menu>
-      <UserInfo app={app} dispatch={dispatch}/>
+      {/* <UserInfo app={app} dispatch={dispatch}/> */}
     </Header>
-  )
-}
+  );
+};
 
 MHeader.propTypes = {
   app: PropTypes.object,
   location: PropTypes.object,
-  dispatch: PropTypes.func,
-}
+  dispatch: PropTypes.func
+};
 
-export default MHeader
+export default MHeader;
--- src/models/app.js
+++ src/models/app.js
@@ -1,26 +1,26 @@
-import qs from 'qs'
-import { routerRedux } from 'dva/router'
-import config from 'config'
-import { message } from 'antd'
+import qs from "qs";
+import { routerRedux } from "dva/router";
+import config from "config";
+import { message } from "antd";
 import {
   getMenu,
   query,
   loginout,
   queryUserInfo,
-  modifyUserInfo,
-} from 'services/app'
+  modifyUserInfo
+} from "services/app";
 
 export default {
-  namespace: 'app',
+  namespace: "app",
   state: {
-    locationPathname: '',
+    locationPathname: "",
     locationQuery: {},
     menu: [
       {
-        id: '1',
-        icon: 'dashboard',
-        name: '首页',
-        route: '/dashboard',
+        id: "1",
+        icon: "dashboard",
+        name: "首页",
+        route: "/dashboard"
       },
 
       /*
@@ -32,148 +32,152 @@ export default {
       },*/
 
       {
-        id: '4',
-        name: 'GIS信息',
-        icon: 'camera-o',
-        route: '/gis',
+        id: "4",
+        name: "GIS信息",
+        icon: "camera-o",
+        route: "/gis"
       },
       {
-        id: '5',
-        name: '管理策略',
-        icon: 'heart-o',
+        id: "5",
+        name: "管理策略",
+        icon: "heart-o",
         children: [
           {
-            id: '51',
-            bpid: '5',
-            name: '设备管理',
-            icon: 'search',
-            route: '/devicemanage',
+            id: "51",
+            bpid: "5",
+            name: "设备管理",
+            icon: "search",
+            route: "/devicemanage"
           },
           {
-            id: '52',
-            name: '固件管理',
-            icon: 'bars',
-            route: '/firmware',
-          },
+            id: "52",
+            name: "固件管理",
+            icon: "bars",
+            route: "/firmware"
+          }
         ]
       },
 
       {
-        id: '6',
-        name: '系统管理',
-        icon: 'heart-o',
+        id: "6",
+        name: "系统管理",
+        icon: "heart-o",
         children: [
           {
-            id: '53',
-            bpid: '6',
-            name: '用户管理',
-            icon: 'search',
-            route: '/usermanage',
+            id: "53",
+            bpid: "6",
+            name: "用户管理",
+            icon: "search",
+            route: "/usermanage"
           },
           {
-            id: '54',
-            bpid: '6',
-            name: '角色管理',
-            icon: 'search',
-            route: '/manage/role',
-          },
+            id: "54",
+            bpid: "6",
+            name: "角色管理",
+            icon: "search",
+            route: "/manage/role"
+          }
         ]
       }
     ], // 菜单
     user: {
-      userName: 'deadpool'
+      userName: "deadpool"
     }, // 用户信息
-    userInfoModalVisibal: false,
+    userInfoModalVisibal: false
   },
   subscriptions: {
-    setupHistory ({ dispatch, history }) {
-      history.listen((location) => {
-        location.search = location.search || ''
-        const ls = location.search.replace('?', '')
+    setupHistory({ dispatch, history }) {
+      history.listen(location => {
+        location.search = location.search || "";
+        const ls = location.search.replace("?", "");
         dispatch({
-          type: 'updateState',
+          type: "updateState",
           payload: {
             locationPathname: location.pathname,
-            locationQuery: qs.parse(ls),
-          },
-        })
-      })
+            locationQuery: qs.parse(ls)
+          }
+        });
+      });
     },
 
     setup({ dispatch }) {
       if (config.loginLimit) {
-        dispatch({ type: 'query' })
+        dispatch({ type: "query" });
       }
     }
   },
 
   effects: {
-    *getMenu ({ payload }, { call, put, select }) {
-      const resData = yield call(getMenu)
+    *getMenu({ payload }, { call, put, select }) {
+      const resData = yield call(getMenu);
       yield put({
-        type: 'updateState',
+        type: "updateState",
         payload: {
-          menu: resData.data,
+          menu: resData.data
         }
-      })
+      });
     },
 
-    *query ({ payload }, { call, put, select }) {
-      const { locationPathname } = select(_=>_.app)
-      const resData = yield call(query)
+    *query({ payload }, { call, put, select }) {
+      const { locationPathname } = select(_ => _.app);
+      const resData = yield call(query);
       if (resData.success) {
         // 用户已登录
-        if (locationPathname === '/login') {
-          yield put(routerRedux.push('/dashboard'))
+        if (locationPathname === "/login") {
+          yield put(routerRedux.push("/dashboard"));
         }
       } else if (config.openPages.indexOf(locationPathname) < 0) {
-        yield put(routerRedux.push({
-          pathname: '/login',
-          search: qs.stringify({ from: locationPathname }),
-        }))
+        yield put(
+          routerRedux.push({
+            pathname: "/login",
+            search: qs.stringify({ from: locationPathname })
+          })
+        );
       }
     },
 
     // 查询用户信息
-    *queryUserInfo(payload, { call, put, select}) {
-      const { user } = yield select(_=>_.app)
-      const resData = yield call(queryUserInfo, { id: user.id })
+    *queryUserInfo(payload, { call, put, select }) {
+      const { user } = yield select(_ => _.app);
+      const resData = yield call(queryUserInfo, { id: user.id });
       if (resData.success) {
-        yield put({ type: 'updateState', payload: { user: resData.data }})
+        yield put({ type: "updateState", payload: { user: resData.data } });
       } else {
-        message.error(resData.message)
+        message.error(resData.message);
       }
     },
 
     // 修改用户信息
     *modifyUserInfo({ payload }, { call, select }) {
-      const { user } = yield select(_=>_.app)
+      const { user } = yield select(_ => _.app);
       const resData = yield call(modifyUserInfo, {
         ...payload,
-        ...user,
-      })
+        ...user
+      });
 
       if (resData.success) {
-        message.success('修改成功')
+        message.success("修改成功");
       } else {
-        message.error(resData.message)
+        message.error(resData.message);
       }
     },
 
-    *loginout ({ payload }, { call, put }) {
-      yield call(loginout)
-      yield put(routerRedux.push({
-        pathname: '/login',
-      }))
-    },
+    *loginout({ payload }, { call, put }) {
+      yield call(loginout);
+      yield put(
+        routerRedux.push({
+          pathname: "/login"
+        })
+      );
+    }
   },
 
   reducers: {
     updateState: function(state, { payload }) {
       return {
         ...state,
-        ...payload,
-      }
+        ...payload
+      };
     }
   }
-}
+};
